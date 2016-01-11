e2989313bece4ea9972a5f53eaa95f729b44b20c
plough <tzgdaydayup@gmail.com>
2016-01-11 13:35:25 +0800

    增加wasd控制方向；修改音乐和音效

--- game.py
+++ game.py
@@ -7,7 +7,13 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((800, 500))
+
+USIZE = 20 # 单位长度
+ROWS = 25
+COLUMNS = 25
+size = USIZE * COLUMNS + 300, USIZE * ROWS
+
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption('mysnake 1.0')
 
 # set up the colors
@@ -15,9 +21,6 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 #
-USIZE = 20 # 单位长度
-ROWS = 25
-COLUMNS = 25
 GAMESTATE = 'playing'
 score = 0
 isFruitShowing = False
@@ -34,10 +37,13 @@ scoreFont = pygame.font.Font('freesansbold.ttf', 32)
 
 # 背景音乐
 pygame.mixer.init()
-pygame.mixer.music.load('background.mp3')
+pygame.mixer.music.load('res/background.mp3')
+#  pygame.mixer.music.load('res/bg.mp3')
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
-soundEat = pygame.mixer.Sound('eat.ogg')
+soundEat = pygame.mixer.Sound('res/eat.ogg')
+soundFail = pygame.mixer.Sound('res/gameover.ogg')
+soundJiayou = pygame.mixer.Sound('res/jiayou.ogg')
 
 # 辅助函数
 def initGame():
@@ -53,9 +59,9 @@ def initGame():
     pygame.mixer.music.rewind()
     pygame.mixer.music.unpause()
 def drawFinal():
-    pygame.draw.rect(DISPLAYSURF, RED, \
+    pygame.draw.rect(screen, RED, \
             (200, 120, 400, 300))
-    pygame.draw.rect(DISPLAYSURF, BLACK, \
+    pygame.draw.rect(screen, BLACK, \
             (210, 130, 380, 280))
     overText = scoreFont.render('GAME OVER!',\
             True, WHITE)
@@ -63,9 +69,9 @@ def drawFinal():
             True, WHITE)
     promptText = fontObj.render('Press "ENTER" to RESTART',
             True, WHITE)
-    DISPLAYSURF.blit(overText, (300, 200))
-    DISPLAYSURF.blit(scoreText, (300, 240))
-    DISPLAYSURF.blit(promptText, (280, 290))
+    screen.blit(overText, (300, 200))
+    screen.blit(scoreText, (300, 240))
+    screen.blit(promptText, (280, 290))
 
 def drawFruit():
     """生成并绘制食物"""
@@ -77,25 +83,25 @@ def drawFruit():
             fY = random.randint(0, ROWS-1)
             tempPos = (fX, fY)
         fruitPos = tempPos
-    pygame.draw.rect(DISPLAYSURF, RED, \
+    pygame.draw.rect(screen, RED, \
             (fruitPos[0]*USIZE, fruitPos[1]*USIZE, USIZE, USIZE))
     isFruitShowing = True
 
 def drawSnake():
     for pos in snake.bodyList:
-        pygame.draw.rect(DISPLAYSURF, WHITE, \
+        pygame.draw.rect(screen, WHITE, \
                 (pos[0]*USIZE, pos[1]*USIZE, USIZE, USIZE))
 
 def redraw():
-    DISPLAYSURF.fill(BLACK)
+    screen.fill(BLACK)
     # 分割线
-    pygame.draw.line(DISPLAYSURF, RED, (502, 0), (502, 500), 3)
+    pygame.draw.line(screen, RED, (502, 0), (502, 500), 3)
     promptText = fontObj.render('Press "SPACE" to', True, WHITE)
     promptText2 = fontObj.render('START/PAUSE', True, WHITE)
     scoreText = scoreFont.render('Score: ' + str(score), True, WHITE)
-    DISPLAYSURF.blit(promptText, (550, 100))
-    DISPLAYSURF.blit(promptText2, (560, 120))
-    DISPLAYSURF.blit(scoreText, (570, 220))
+    screen.blit(promptText, (550, 100))
+    screen.blit(promptText2, (560, 120))
+    screen.blit(scoreText, (570, 220))
     drawSnake()
     drawFruit()
 
@@ -116,6 +122,9 @@ while True:
         isLocked = False
     for event in pygame.event.get():
         if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                sys.exit()
+
             if GAMESTATE == 'over' and event.key == K_RETURN:
                 print 'Return press'
                 initGame()
@@ -128,19 +137,19 @@ while True:
                     pygame.mixer.music.unpause()
             if GAMESTATE == 'playing' and not isLocked:
                 newDirection = ''
-                if event.key == K_DOWN:
+                if event.key in (K_DOWN, K_s):
                     newDirection = 'down'
-                elif event.key == K_UP:
+                elif event.key in (K_UP, K_w):
                     newDirection = 'up'
-                elif event.key == K_LEFT:
+                elif event.key in (K_LEFT, K_a):
                     newDirection = 'left'
-                elif event.key == K_RIGHT:
+                elif event.key in (K_RIGHT, K_d):
                     newDirection = 'right'
                 if snake.isValidDirection(newDirection):
                     snake.changeDirection(newDirection)
                     isLocked = True
         if event.type == QUIT:
-            pygame.quit()
+            #  pygame.quit()
             sys.exit()
     if GAMESTATE == 'playing':
         result = checkCollision()
@@ -152,8 +161,12 @@ while True:
             isFruitShowing = False
             if speed < 15:
                 speed *= 1.1
+            if score in (30, 50, 65, 75) or\
+                (score > 75 and (score - 80) % 5 == 0):
+                soundJiayou.play()
         elif result == -1:
             GAMESTATE = 'over'
+            soundFail.play()
             drawFinal()
         redraw()
     elif GAMESTATE == 'over':
